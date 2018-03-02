1f33eb59c37e2fc937f3eea06b50562a322cb732
Jessica Kerr <jessitron@gmail.com>
2018-03-02 15:37:44 -0600

    Initial commit from Atomist

---
+++ src/main/java/doo/be/doo/DoobedooApplication.java
@@ -0,0 +1,12 @@
+package doo.be.doo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoobedooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoobedooApplication.class, args);
+	}
+}
